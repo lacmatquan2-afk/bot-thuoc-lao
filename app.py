12abd8a0e3638e824211ed605db670bf3a9b4507
lacmatquan2-afk <lacmatquan2@gmail.com>
2026-02-28 06:18:37 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,28 @@
 from flask import Flask, request
-app = Flask(__name__)
+
+app = Flask(_name_)
+
 VERIFY_TOKEN = "thuoclao123"
+
 @app.route("/")
 def home():
-      return "Bot thuốc lào Quảng Định đang chạy!"
-@app.route("wedhook", methods=["GET", "POST"])
-def wedhook():
-      if request.method =="GET":
-            token = request.args.get("hub.verify_token")
-            challenge = request.args.get("hub.challenge")
-            if token == VERIFY_TOKEN:
-                  return challenge
-             alse:
-                  return "Verify token mismatch", 403
-       if request.method == "POST":
-             data = request.get_json()
-             print(data)
-             return "OK", 200
-if __name__ == "__main__":
+    return "Bot thuốc lào Quảng Định đang chạy!"
+
+@app.route("/webhook", methods=["GET", "POST"])
+def webhook():
+    if request.method == "GET":
+        token = request.args.get("hub.verify_token")
+        challenge = request.args.get("hub.challenge")
+
+        if token == VERIFY_TOKEN:
+            return challenge
+        else:
+            return "Verify token mismatch", 403
+
+    if request.method == "POST":
+        data = request.get_json()
+        print(data)
+        return "OK", 200
+
+if _name_ == "_main_":
     app.run()
