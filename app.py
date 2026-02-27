61557da9ba1583931560d9b062a60b15996997eb
lacmatquan2-afk <lacmatquan2@gmail.com>
2026-02-28 05:19:16 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask
+import os
+
 app = Flask(__name__)
 @app.route("/")
 def home():
       return "Bot thuốc lào Quảng Định đang chạy!"
 if __name__ == "__main__":
-      app.run(host="0.0.0.0",port=10000)
+      port = int(os.environ.get("PORT", 10000)
+      app.run(host="0.0.0.0",port=port)
+
