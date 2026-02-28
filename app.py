52fd9cfd426017be09bc96eee29e11c4a82b0c49
lacmatquan2-afk <lacmatquan2@gmail.com>
2026-02-28 07:38:55 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+import os
 
 app = Flask(__name__)
 
@@ -25,3 +26,4 @@ def webhook():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
+
