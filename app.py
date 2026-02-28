25511de655c007d8df2dcc9afd2843696623a5a4
lacmatquan2-afk <lacmatquan2@gmail.com>
2026-02-28 07:43:53 +0700

    Update app.py

--- app.py
+++ app.py
@@ -12,18 +12,19 @@ def home():
 @app.route("/webhook", methods=["GET", "POST"])
 def webhook():
     if request.method == "GET":
-        mode = request.args.get("hub.mode")
-        token = request.args.get("hub.verify_token")
-        challenge = request.args.get("hub.challenge")
+    mode = request.args.get("hub_mode")
+    token = request.args.get("hub_verify_token")
+    challenge = request.args.get("hub_challenge")
 
-        if mode == "subscribe" and token == "thuoclao123":
-            return challenge, 200
-        else:
-            return "Verify token mismatch", 403
+    if mode == "subscribe" and token == VERIFY_TOKEN:
+        return challenge, 200
+    else:
+        return "Verify token mismatch", 403
 
     if request.method == "POST":
         return "EVENT_RECEIVED", 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
+
 
